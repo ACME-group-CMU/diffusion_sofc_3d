b4b188b7a573bbf8eb82dba51e804aaae3d91ed2
rochan17 <rbajpai@andrew.cmu.edu>
2023-11-16 15:06:27 -0500

    Added upsampling instead of convTranspose

--- submit.sh
+++ submit.sh
@@ -20,4 +20,4 @@ cd ~/Diffusion
 
 ##export NCCL_DEBUG=INFO
 
-srun python3 main.py --n_epochs 200 --batch_size 48  --n_nodes 12 --img_size 96 --sample_interval 20 --data_length 40000 --lr 0.0001 --dif_timesteps 200 --inf_timesteps 50 --base_dim 16 --n_blocks 2 --sample_size 4 --apply_sym False --n_cpu 4 --mse_sum False --n_gpu 1 --is_attn "(0,0,1,1)" --ch_mul "(1,4,4,1)" --scheduler_gamma 0.985 
+srun python3 main.py --n_epochs 200 --batch_size 48  --n_nodes 12 --img_size 96 --sample_interval 20 --data_length 40000 --lr 0.0001 --dif_timesteps 1000 --inf_timesteps 50 --base_dim 16 --n_blocks 2 --sample_size 4 --apply_sym False --n_cpu 4 --mse_sum False --n_gpu 1 --is_attn "(0,0,1,1)" --ch_mul "(1,4,4,1)" --scheduler_gamma 0.985 
