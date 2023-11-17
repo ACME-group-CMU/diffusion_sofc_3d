d458fc4431109a3c44826f99cb15cb04ee410a7d
rochan17 <rbajpai@andrew.cmu.edu>
2023-11-16 21:58:22 -0500

    ignore all npz files so that no data is tracked.

--- submit.sh
+++ submit.sh
@@ -17,7 +17,7 @@ module load anaconda3/2021.05
 source activate rbenv
 
 cd ~/Diffusion
-
+git checkout upsample
 ##export NCCL_DEBUG=INFO
 
-srun python3 main.py --n_epochs 200 --batch_size 48  --n_nodes 12 --img_size 96 --sample_interval 20 --data_length 40000 --lr 0.0001 --dif_timesteps 1000 --inf_timesteps 50 --base_dim 16 --n_blocks 2 --sample_size 4 --apply_sym False --n_cpu 4 --mse_sum False --n_gpu 1 --is_attn "(0,0,1,1)" --ch_mul "(1,4,4,1)" --scheduler_gamma 0.985 
+srun python3 main.py --n_epochs 100 --batch_size 48  --n_nodes 12 --img_size 96 --sample_interval 20 --data_length 40000 --lr 0.0001 --dif_timesteps 1000 --inf_timesteps 50 --base_dim 16 --n_blocks 2 --sample_size 4 --apply_sym False --n_cpu 4 --mse_sum False --n_gpu 1 --is_attn "(0,0,1,1)" --ch_mul "(1,4,4,1)" --scheduler_gamma 0.985 
